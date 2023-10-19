4b1ca108d9b332b596eaee461637efe1a9bc167e
Minong <127920459+Minong26@users.noreply.github.com>
2023-10-19 18:17:38 +0900

    Sprites Marge

---
+++ Assets/01_Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 2.0f;
+    public float jumpForce = 20.0f;
+
+    Rigidbody2D rigi;
+
+    private void Start()
+    {
+        rigi = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        transform.Translate(new Vector3(horizontal, 0).normalized * moveSpeed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rigi.AddForce(new Vector3(0, jumpForce, 0));
+        }
+    }
+}
